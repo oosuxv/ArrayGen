f2c1eaea81b3c13e7ab7f3997cb3014d933e34aa
Vadim <supskotom@yandex.ru>
2022-02-04 12:23:42 +0700

    KISS

--- src/ArrayGenerator.java
+++ src/ArrayGenerator.java
@@ -1,29 +1,27 @@
 import java.util.*;
 
 public class ArrayGenerator {
-    private static int ARRAY_STEP_LIMIT = 5;
     private static int NUMBER_LIMIT = 1000;
 
-    public int[][] generateArray(int outerSize) {
+    public Integer[][] generateArray(int outerSize) {
         if (outerSize <= 0) {
             throw new IllegalArgumentException("Size should be positive integer");
         }
-        int[][] data = populateArray(outerSize);
+        Integer[][] data = populateArray(outerSize);
         sortAlternate(data);
         return data;
     }
 
-    private int[][] populateArray(int outerSize) {
-        int[][] data = new int[outerSize][];
+    private Integer[][] populateArray(int outerSize) {
+        Integer[][] data = new Integer[outerSize][];
         Random random = new Random();
         List<Integer> innerSizes = new ArrayList<>(outerSize);
-        innerSizes.add(random.nextInt(1, ARRAY_STEP_LIMIT));
-        for (int i = 1; i < outerSize; i++) {
-            innerSizes.add(innerSizes.get(i - 1) + random.nextInt(1, ARRAY_STEP_LIMIT));
+        for (int i = 1; i <= outerSize; i++) {
+            innerSizes.add(i);
         }
         Collections.shuffle(innerSizes);
         for (int i = 0; i < outerSize; i++) {
-            data[i] = new int[innerSizes.get(i)];
+            data[i] = new Integer[innerSizes.get(i)];
             for (int j = 0; j < data[i].length; j++) {
                 data[i][j] = random.nextInt(NUMBER_LIMIT);
             }
@@ -31,21 +29,13 @@ public class ArrayGenerator {
         return data;
     }
 
-    private void sortAlternate(int[][] data) {
+    private void sortAlternate(Integer[][] data) {
         for (int i = 0; i < data.length; i++) {
-            Arrays.sort(data[i]);
             if (i % 2 == 0) {
-                reverseArray(data[i]);
+                Arrays.sort(data[i], Collections.reverseOrder());
+            } else {
+                Arrays.sort(data[i]);
             }
-        }
-    }
-
-    private void reverseArray(int[] datum) {
-        for (int j = 0, temp, antiJ; j < datum.length / 2; j++) {
-            antiJ = datum.length - j - 1;
-            temp = datum[j];
-            datum[j] = datum[antiJ];
-            datum[antiJ] = temp;
         }
     }
 }
